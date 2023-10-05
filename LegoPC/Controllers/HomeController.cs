6779d33e4e8c0fda05f089c4eecc0d66f081437e
DenysWindows <->
2023-10-05 16:11:51 +0300

    1

---
+++ LegoPC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using LegoPC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LegoPC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        AccessoryContext db = new AccessoryContext();
+        public ActionResult Index()
+        {
+            // Отримуємо з БД всі об'єкти Book
+            IEnumerable<Accessory> accessories = db.Accessories;
+            // Передаємо всі об'єкти в динамічну властивість Books в ViewBag
+            ViewBag.Accessories = accessories;
+            // Повертаємо представлення
+            return View();
+        }
+    }
+}
---
+++ LegoPC/Models/AccessoryDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LegoPC.Models
+{
+    public class AccessoryDbInitializer : DropCreateDatabaseAlways<AccessoryContext>
+    {
+        protected override void Seed(AccessoryContext db)
+        {
+            db.Accessories.Add(new Accessory { Name = "Война и мир", Type = "Л.Толстой", Price = 220 });
+            db.Accessories.Add(new Accessory { Name = "Отцы и дети", Type = "И.Тургенев", Price = 180 });
+            db.Accessories.Add(new Accessory { Name = "Чайка", Type = "А. Чехов", Price = 150 });
+            base.Seed(db);
+        }
+    }
+}
